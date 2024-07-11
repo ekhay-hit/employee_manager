055e898ad87380836c4e8dedea9b825d2fe58cfa
Houssine <ekhay1@outlook.com>
2024-07-10 20:16:12 -0400

    added env file for securing credintials and used in connection file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,14 @@
+// require Sequelize
 const Sequelize = require("sequelize");
+// require dotenv for data base credintials 
+require('dotenv').config();
 
 // initialize connect configuration
-const sequelize = new Sequelize("employee_db","postgres", "ad12",{
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
     host: 'localhost',
     dialect: 'postgres',
 });
